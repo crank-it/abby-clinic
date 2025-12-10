8bab4298cf69eb4904cf1c7b875beeec4f8af5fc
Rod-Yoonet <dev@yoonet.io>
2025-12-10 08:06:24 +0800

    GTM

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -8,6 +8,10 @@ interface MarketingConfig {
     measurementId: string;
     snippet: string;
   };
+  gtm: {
+    enabled: boolean;
+    containerId: string;
+  };
   googleSearchConsole: {
     enabled: boolean;
     verificationTag: string;
@@ -97,6 +101,20 @@ export async function TrackingScripts() {
             fbq('track', 'PageView');
           `}
         </Script>
+      )}
+
+      {/* Google Tag Manager */}
+      {config.gtm?.enabled && config.gtm.containerId && (
+        <>
+          <Script id="gtm-init" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'? '&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${config.gtm.containerId}');`}
+          </Script>
+
+          {/* noscript iframe must be present immediately after opening <body> */}
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${config.gtm.containerId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }} />
+        </>
       )}
     </>
   );
