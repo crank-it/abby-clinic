6de86a98ddc4ca6b9f871d6e1d061d64a533a156
Rod-Yoonet <dev@yoonet.io>
2025-12-10 08:40:13 +0800

    Move GTM to head

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -103,22 +103,11 @@ export async function TrackingScripts() {
         </Script>
       )}
 
-      {/* Google Tag Manager */}
+      {/* Google Tag Manager noscript fallback (script is in head via layout.tsx) */}
       {config.gtm?.enabled && config.gtm.containerId && (
-        <>
-          <Script id="gtm-init" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MQNC7Q9F');`}
-          </Script>
-
-          {/* noscript iframe must be present immediately after opening <body> */}
-          <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQNC7Q9F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-          }} />
-        </>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${config.gtm.containerId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
       )}
     </>
   );
