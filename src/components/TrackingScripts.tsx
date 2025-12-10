7e0f8ea79ac90c6037432a735a7222de0bd29be4
Rod-Yoonet <dev@yoonet.io>
2025-12-10 08:29:26 +0800

    dipluy

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -111,12 +111,12 @@ export async function TrackingScripts() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${config.gtm.containerId}');`}
+})(window,document,'script','dataLayer','GTM-MQNC7Q9F');`}
           </Script>
 
           {/* noscript iframe must be present immediately after opening <body> */}
           <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${config.gtm.containerId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQNC7Q9F" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
           }} />
         </>
       )}
